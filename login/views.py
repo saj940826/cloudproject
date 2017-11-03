7cc954396327a6d71bcc5422e647326cab29223f
Anjie Song <saj940826163@gmail.com>
2017-11-03 05:39:01 +0000

    add template v1

--- login/views.py
+++ login/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+
 from django.http import HttpResponse
+from django.template import loader
 
 def index(request):
-    return HttpResponse("Hello, world.")
+    template = loader.get_template('login/home.html')
+    context = {}
+    return HttpResponse(template.render(context, request))
+
 # Create your views here.
